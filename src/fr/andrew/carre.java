8c3ca01c74ca644a4827bfbdb0e7e5cf761bfbfd
Andrew <andr3wcarpentier@gmail.com>
2018-10-11 12:46:24 +0200

    Modification sur carre.java

--- src/fr/andrew/carre.java
+++ src/fr/andrew/carre.java
@@ -22,29 +22,16 @@ public class carre {
 		
 		System.out.println("nombreEtoileParCote");
 		nombreEtoileParCote = sc.nextInt();;
-		
-		for(int i = 0 ; i < nombreEtoileParCote ; i++) {
-			if(i == 0){
-				for(int j = 0; j < nombreEtoileParCote ; j++) {
+
+		for(int i = 0 ; i < nombreEtoileParCote; i++) {
+			for(int j = 0 ; j < nombreEtoileParCote; j++) {
+				if(i == 0 || i ==(nombreEtoileParCote - 1) || j == 0 || j == (nombreEtoileParCote-1)) {
 					System.out.print("* ");
+				}else {
+					System.out.print("  ");
 				}
-				System.out.println();
-			}else if(i == (nombreEtoileParCote-1)) {
-				for(int j = 0; j < nombreEtoileParCote ; j++) {
-					System.out.print("* ");
-				}
-				System.out.println();
-			}else if( i != 0 || i != (nombreEtoileParCote-2)) {
-				for(int j = 0; j < nombreEtoileParCote ; j++) {
-					if(j == 0 || j == (nombreEtoileParCote-1)) {
-						System.out.print("* ");
-					}else {
-						System.out.print("  ");
-					}					
-				}
-				System.out.println();
 			}
+			System.out.println();			
 		}
-		
 	}
 }
